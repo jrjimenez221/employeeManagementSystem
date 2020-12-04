c31189219f92c00a728d50efc8e156095a93c6e4
Jose Jimenez <jrjimenez221@gmail.com>
2020-12-04 08:25:11 -0600

    added tables

---
+++ employeeTrackerDB.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS employeeTrackerDB;
+CREATE DATABASE employeeTrackerDB;
+
+USE employeeTrackerDB;
+
+CREATE TABLE employees(
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(45) NOT NULL,
+  role_id INT,
+  manager_id INT,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role(
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30),
+  salary INT,
+  PRIMARY KEY (id)
+)
+
+CREATE TABLE department(
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30),
+  PRIMARY KEY (id)
+)
